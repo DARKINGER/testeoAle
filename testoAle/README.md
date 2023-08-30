526e64ad7fcec0ffd291ab0979432338f670bc04
DARKINGER <alexanfmdm@gmail.com>
2023-08-30 08:10:37 -0500

    Primero

---
+++ testoAle/README.md
@@ -0,0 +1,12 @@
+# modulo de prueba
+
+
+instalacion de modulo:
+```bash
+  $npm install testeoAle
+```
+uso del modulo:
+```js
+const testeoAle=require('testeoAle');
+let frase=testeoAle
+```
---
+++ testoAle/index.js
@@ -0,0 +1,15 @@
+let frases = [
+"frase 1",
+"frase 2",
+"frase 3"
+]
+
+
+function obtienefrase(indice){
+    return frases(indice); 
+}
+
+console.log(module);
+
+module.exports.obtienefrase=obtienefrase;
+
